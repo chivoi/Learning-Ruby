c9616ef9b42f12ca53ce460bdb41bb8ad61b76c3
chivoi <a.lastovirya@gmail.com>
2021-01-27 10:30:00 +1100

    added recursive implementation of binary search to binary-search.rb

--- binary-search.rb
+++ binary-search.rb
@@ -6,6 +6,27 @@
 # binary_search([1,5,8,12,20,21,35], 8) # should return [2, 3] 
 # In this case the index returned would be 2 and it should take 3 steps. 
 
+
+#recursive implementation
+def binary_search(arr, value, steps=0, index=[])
+  index << arr.index(value)
+  midpoint = arr.length/2
+  steps += 1
+  
+  if arr.empty? || arr.include?(value) == false
+    return "Value is not in the array"
+  else
+    if arr[midpoint] == value
+      return result(index[0], steps)
+    elsif arr[midpoint] > value
+      binary_search(arr[0..(midpoint-1)], value, steps, index)
+    else arr[midpoint] < value
+      binary_search(arr[(midpoint+1)..(-1)], value, steps, index)
+    end 
+  end
+end
+
+# loop implementation
 def binary_search(arr, value)
 	index = arr.index(value)
 	steps = 0
@@ -33,7 +54,6 @@ def binary_search(arr, value)
 end
 
 # helper to get result array from index and steps
-
 def result (index, steps)
 	result = []
 	result << index
